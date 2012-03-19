a4758b8382dcaaa58f1a65d6408463fe71497f06
Mariano Guerra <luismarianoguerra@gmail.com>
2012-03-19 19:51:25 +0100

    don't generate the minified version anymore

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@
 jison src/legoparser.jison -o src/legoparser.js
 cat templates/jquery.lego.web.start.js src/jquery.lego.js src/legoparser.js templates/jquery.lego.web.end.js > jquery.lego.web.js
 cat templates/jquery.lego.require.start.js src/jquery.lego.js src/legoparser.js templates/jquery.lego.require.end.js > jquery.lego.requirejs.js   
-uglifyjs jquery.lego.web.js > jquery.lego.web.min.js
