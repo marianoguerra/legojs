6defcd1a37c277cede6d012602e24f85bc789f5a
Mariano Guerra <luismarianoguerra@gmail.com>
2012-03-20 10:47:21 +0100

    add more examples

--- js/main.js
+++ js/main.js
@@ -29,6 +29,22 @@ $(function () {
     $("#load").click(onLoadClick);
 
     snippets = {
+        "tag": "p",
+        "tag with content": "p {hello world}",
+        "tag with id": "p#myid",
+        "tag with class": "p.important",
+        "tag with classes": "p.important.first",
+        "tag with id and classes": "p#intro.important.first",
+        "tag with id, classes and content": "p#intro.important.first {hi}",
+        "just id": "#wrapper",
+        "just class": ".paragraph",
+        "id and class": "#wrapper.paragraph",
+        "child": "li > a {click}",
+        "attribute": 'li > a[href="http://marianoguerra.com.ar"] {click}',
+        "attributes": 'img[src="img/lego.png" alt="lego"]',
+        "repetition": "#wrapper > p * 3 {hi}",
+        "id enumeration": "#wrapper > p#item-$ * 3",
+        "siblings and grouping": "#wrapper > ({welcome } + em {username} + strong {!})",
         "page title": 'h1>(img[src="img/lego.png" alt="lego"] + {js})',
         "first paragraph": 'p > ({write a } + em {legojs} + { expression below and click } + strong {run})',
         "list items": 'ul > li#item-$ * 3 {list item}'
