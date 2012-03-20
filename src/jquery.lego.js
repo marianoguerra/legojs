266d323c33218ea8d9d4759dda9fce50ac6bf16b
Mariano Guerra <luismarianoguerra@gmail.com>
2012-03-20 10:27:36 +0100

    remove utility functions

--- src/jquery.lego.js
+++ src/jquery.lego.js
@@ -96,84 +96,11 @@
                                 $tag.attr(key, options[key]);
                             }
                         }
-
                     }
                 }
             }
 
             return $tag;
         }
-
     };
 
-    function addIfSet(obj, fields) {
-        var key;
-
-        for (key in fields) {
-            if (fields[key]) {
-                obj[key] = fields[key];
-            }
-        }
-
-        return obj;
-    }
-
-    function link(href, body, id, cls) {
-        return {"a": addIfSet({"href": href, "$childs": body}, {"id": id, "class": cls})};
-    }
-
-    function input(type, value, id, cls) {
-        return {"input": addIfSet({"type": type, "value": value || ""}, {"id": id, "class": cls})};
-    }
-
-    function text(value, id, cls) {
-        return input("text", value, id, cls);
-    }
-
-    function button(label, id, cls) {
-        return {"button": addIfSet({"$childs": label}, {"id": id, "class": cls})};
-    }
-
-    function img(src, alt) {
-        return {"img": {"src": src, "alt": alt}};
-    }
-
-    function li(body) {
-        return {"li": {"$childs": body}};
-    }
-
-    function imglink(href, src, alt, id, cls) {
-        return link(href, img(src, alt), id, cls);
-    }
-
-    function tag(name, body, id, cls) {
-        var base = {};
-        base[name] = addIfSet({"$childs": body}, {"id": id, "class": cls});
-        return base;
-    }
-
-    function div(body, id, cls) {
-        return tag("div", body, id, cls);
-    }
-
-    function span(body, id, cls) {
-        return tag("div", body, id, cls);
-    }
-
-    if (window.console && console.error) {
-        $.lego.error = console.error;
-    } else {
-        $.lego.error = function () {};
-    }
-
-    l = $.lego;
-
-    l.link = link;
-    l.img = img;
-    l.li = li;
-    l.imglink = imglink;
-    l.div = div;
-    l.span = span;
-    l.input = input;
-    l.text = text;
-    l.button = button;
